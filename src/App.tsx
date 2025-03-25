da09ed996ef5e10ba0570648b50ad443d5080703
AslamThachapalli <aslam.develop912@gmail.com>
2025-03-25 21:03:26 +0530

    mint token creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,89 @@
-function App() {
+import {
+    createInitializeMint2Instruction,
+    getMinimumBalanceForRentExemptMint,
+    MINT_SIZE,
+    TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import {
+    Keypair,
+    SystemProgram,
+    Transaction,
+} from "@solana/web3.js";
 
-  return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '10px',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-      width: '100vw',
-    }}>
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '10px',
-      }}>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Symbol" />
-        <input type="text" placeholder="Decimals" />
-        <input type="text" placeholder="Supply" />
-        <input type="text" placeholder="Description" />
-        <input type="text" placeholder="Image URL" />
-      </div>
-      <div>
-        <button>Create Token</button>
-      </div>
-    </div>
-  )
+function App() {
+    const wallet = useWallet();
+    const { connection } = useConnection();
+
+    const handleCreateToken = async () => {
+        if (!wallet.connected) {
+            return;
+        }
+
+        const keypair = Keypair.generate();
+
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey!,
+                newAccountPubkey: keypair.publicKey,
+                space: MINT_SIZE,
+                lamports,
+                programId: TOKEN_PROGRAM_ID,
+            }),
+            createInitializeMint2Instruction(
+                keypair.publicKey,
+                6,
+                wallet.publicKey!,
+                null,
+                TOKEN_PROGRAM_ID
+            )
+        );
+
+        transaction.feePayer = wallet.publicKey!;
+        transaction.recentBlockhash = (
+            await connection.getLatestBlockhash()
+        ).blockhash;
+        transaction.partialSign(keypair);
+
+        const txHash = await wallet.sendTransaction(transaction, connection);
+        console.log(txHash);
+    };
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+                width: "100vw",
+            }}
+        >
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "10px",
+                }}
+            >
+                <WalletMultiButton />
+                <input type="text" placeholder="Name" />
+                <input type="text" placeholder="Symbol" />
+                <input type="text" placeholder="Decimals" />
+                <input type="text" placeholder="Supply" />
+                <input type="text" placeholder="Description" />
+                <input type="text" placeholder="Image URL" />
+            </div>
+            <div>
+                <button onClick={handleCreateToken}>Create Token</button>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+    ConnectionProvider,
+    WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import '@solana/wallet-adapter-react-ui/styles.css';
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <ConnectionProvider endpoint={`https://solana-devnet.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY_API_KEY}`}>
+            <WalletProvider wallets={[]} autoConnect>
+                <WalletModalProvider>
+                    <App />
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    </StrictMode>
+);
